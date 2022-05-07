14c7f09337616e5f667620c4b504bdc473c1d9cc
NaokiYazawa <yazawa.naoki.01@gmail.com>
2022-05-07 23:37:19 +0900

    added authentication functionality using NextAuth.js

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,22 @@
+import { NextApiHandler } from 'next';
+import NextAuth from 'next-auth';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import GitHubProvider from 'next-auth/providers/github';
+import prisma from '../../../lib/prisma';
+
+const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
+export default authHandler;
+
+const options = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.NEXT_PUBLIC_GITHUB_ID,
+      clientSecret: process.env.NEXT_PUBLIC_GITHUB_SECRET,
+    }),
+  ],
+  adapter: PrismaAdapter(prisma),
+  secret: process.env.NEXT_PUBLIC_SECRET,
+  pages: {
+    signIn: '/auth/login',
+  },
+};
